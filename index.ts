7291114e8b7321ffbe11847c4683517c94c9e26b
warsan <warsan00@mail.ru>
2022-05-17 17:52:31 +0500

    2 files modified

--- index.ts
+++ index.ts
@@ -4,14 +4,14 @@ import './styles.css';
 // Откройте репозиторий RealWorld Angular в новом окне на StackBlitz
 // https://github.com/gothinkster/angular-realworld-example-app
 window['openGithubProject'] = () => {
-  sdk.openGithubProject('gothinkster/angular-realworld-example-app');
+  sdk.openGithubProject('https://try.gitea.io/warsan/sdk-github-project-sandro');
 }
 
 // Вставьте репозиторий RealWorld Angular на эту страницу
 window['embedGithubProject'] = () => {
   sdk.embedGithubProject(
     'myDiv',
-    'gothinkster/angular-realworld-example-app',
+    'https://try.gitea.io/warsan/sdk-github-project-sandro',
     { height: 600 }
   );
 }
